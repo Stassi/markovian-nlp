e56b2b0373696eee58e86c0683ce914679c624ec
Andreas Stassivik <andreas@stassi.net>
2018-09-21 01:23:30 -0700

    +bigrams

---
+++ test/bigrams.js
@@ -0,0 +1,23 @@
+import { expect } from 'chai';
+import bigrams from '../src/bigrams';
+
+describe('#bigrams', () => {
+  const sentence = 'First the word, then the last. First, last. Sometimes the word.';
+
+  it('should return bigrams with count', () => {
+    expect(bigrams(sentence)).to.include.deep.members([
+      {
+        count: 2,
+        normal: 'the word',
+      },
+      {
+        count: 1,
+        normal: 'sometimes the',
+      },
+      {
+        count: 1,
+        normal: 'the last',
+      },
+    ]);
+  });
+});
