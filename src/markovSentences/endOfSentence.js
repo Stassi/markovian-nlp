4895cf9701c56ba4979b26f6e9cc6a9a30ca8aca
Andreas Stassivik <andreas@stassi.net>
2018-09-28 03:16:11 -0700

    support distributions without end weight

--- src/markovSentences/endOfSentence.js
+++ src/markovSentences/endOfSentence.js
@@ -2,15 +2,17 @@ import {
   applySpec,
   omit,
   pipe,
-  prop,
+  propOr,
   sum,
   values,
 } from 'ramda';
 import weightedRandom from './weightedRandom';
 
+const propOrZero = propOr(0);
+
 const end = '_end';
 const omitEnd = omit([end]);
-const endWeight = prop(end);
+const endWeight = propOrZero(end);
 
 const continueWeight = pipe(
   omitEnd,
--- test/markovSentences/endOfSentence.js
+++ test/markovSentences/endOfSentence.js
@@ -2,18 +2,29 @@ import { expect } from 'chai';
 import endOfSentence from '../../src/markovSentences/endOfSentence';
 
 describe('#endOfSentence', () => {
-  const sentenceEnded = endOfSentence({
+  const threeOutcomes = endOfSentence({
     _end: 1,
     alpha: 1,
     beta: 1,
   });
 
   it('should return deterministic values when provided known seeds', () => {
-    expect(sentenceEnded(1)).to.be.false;
-    expect(sentenceEnded(5)).to.be.true;
+    expect(threeOutcomes(1)).to.be.false;
+    expect(threeOutcomes(5)).to.be.true;
   });
 
   it('should return nondeterministic boolean without seed', () => {
-    expect(sentenceEnded()).to.be.a('boolean');
+    expect(threeOutcomes()).to.be.a('boolean');
+  });
+
+  describe('distributions without an end weight', () => {
+    const withoutEnd = endOfSentence({
+      alpha: 1,
+      beta: 1,
+    });
+
+    it('should return false for distributions without an end weight', () => {
+      expect(withoutEnd()).to.be.false;
+    });
   });
 });
