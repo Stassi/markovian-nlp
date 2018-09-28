e84243b2fb8111492bcce5d7f755df5b03144979
Andreas Stassivik <andreas@stassi.net>
2018-09-28 02:31:59 -0700

    -erroneous test description

--- test/markovSentences/endOfSentence.js
+++ test/markovSentences/endOfSentence.js
@@ -1,21 +1,19 @@
 import { expect } from 'chai';
 import endOfSentence from '../../src/markovSentences/endOfSentence';
 
-describe('markovSentences', () => {
-  describe('#endOfSentence', () => {
-    const sentenceEnded = endOfSentence({
-      _end: 1,
-      alpha: 1,
-      beta: 1,
-    });
+describe('#endOfSentence', () => {
+  const sentenceEnded = endOfSentence({
+    _end: 1,
+    alpha: 1,
+    beta: 1,
+  });
 
-    it('should return deterministic values when provided known seeds', () => {
-      expect(sentenceEnded(1)).to.be.false;
-      expect(sentenceEnded(5)).to.be.true;
-    });
+  it('should return deterministic values when provided known seeds', () => {
+    expect(sentenceEnded(1)).to.be.false;
+    expect(sentenceEnded(5)).to.be.true;
+  });
 
-    it('should return nondeterministic boolean without seed', () => {
-      expect(sentenceEnded()).to.be.a('boolean');
-    });
+  it('should return nondeterministic boolean without seed', () => {
+    expect(sentenceEnded()).to.be.a('boolean');
   });
 });
--- test/markovSentences/followingBigram.js
+++ test/markovSentences/followingBigram.js
@@ -1,20 +1,18 @@
 import { expect } from 'chai';
 import followingBigram from '../../src/markovSentences/followingBigram';
 
-describe('markovSentences', () => {
-  describe('#followingBigram', () => {
-    const balanced = followingBigram({
-      beta: 1,
-      delta: 1,
-    });
+describe('#followingBigram', () => {
+  const balanced = followingBigram({
+    beta: 1,
+    delta: 1,
+  });
 
-    it('should return deterministic values when provided known seeds', () => {
-      expect(balanced(1)).to.equal('beta');
-      expect(balanced(3)).to.equal('delta');
-    });
+  it('should return deterministic values when provided known seeds', () => {
+    expect(balanced(1)).to.equal('beta');
+    expect(balanced(3)).to.equal('delta');
+  });
 
-    it('should return nondeterministic value without seed', () => {
-      expect(balanced()).to.be.a('string');
-    });
+  it('should return nondeterministic value without seed', () => {
+    expect(balanced()).to.be.a('string');
   });
 });
