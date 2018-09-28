1c860e99e224546a3e529fd5edc496c93a775de9
Andreas Stassivik <andreas@stassi.net>
2018-09-27 17:01:50 -0700

    -erroneous test description

--- test/markovSentences/unseededStartgram.js
+++ test/markovSentences/unseededStartgram.js
@@ -1,20 +1,18 @@
 import { expect } from 'chai';
 import unseededStartgram from '../../src/markovSentences/unseededStartgram';
 
-describe('markovSentences', () => {
-  describe('#unseededStartgram', () => {
-    const startgram = unseededStartgram({
-      one: { _start: 1 },
-      three: { _start: 1 },
-    });
+describe('#unseededStartgram', () => {
+  const startgram = unseededStartgram({
+    one: { _start: 1 },
+    three: { _start: 1 },
+  });
 
-    it('should return a deterministic string from distribution when seeded', () => {
-      expect(startgram(1)).to.equal('one');
-      expect(startgram(3)).to.equal('three');
-    });
+  it('should return a deterministic string from distribution when seeded', () => {
+    expect(startgram(1)).to.equal('one');
+    expect(startgram(3)).to.equal('three');
+  });
 
-    it('should return a nondeterministic string from distribution when NOT seeded', () => {
-      expect(startgram()).to.be.a('string');
-    });
+  it('should return a nondeterministic string from distribution when NOT seeded', () => {
+    expect(startgram()).to.be.a('string');
   });
 });
