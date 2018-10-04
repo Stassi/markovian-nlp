23fce3c84567c733906c2c75a8a16e713cba8aa2
Andreas Stassivik <andreas@stassi.net>
2018-10-03 22:17:21 -0700

    (wip) options destructuring

--- src/sentences.js
+++ src/sentences.js
@@ -2,6 +2,7 @@ import {
   identity,
   ifElse,
   is,
+  pipe,
 } from 'ramda';
 import createSentence from './createSentence';
 import ngramsDistribution from './ngramsDistribution';
@@ -11,8 +12,17 @@ const isString = is(String);
 const oneSentence = document => seed =>
   createSentence({ seed, distribution: ngramsDistribution(document) });
 
-// TODO: Implement
-const options = identity;
+// TODO: Implement predicate
+const multipleOut = identity;
+
+const options = pipe(
+  ({ document, seed, count = 1 }) => {
+    const res = { count, document, seed, multipleOut };
+    console.log({ res });
+    // TODO: random[newMethod] seed1 => count => [seed1, seed2, seed3] (length == count)
+    return res;
+  },
+);
 
 const sentences = ifElse(
   isString,
