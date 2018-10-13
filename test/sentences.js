4db6bb96a0314718dfaa63146c1d9b2c366c6a67
Andreas Stassivik <andreas@stassi.net>
2018-10-13 15:50:38 -0700

    disambiguation

--- test/sentences.js
+++ test/sentences.js
@@ -3,29 +3,29 @@ import { sentences } from '../src';
 import sampleDocument from './sampleDocument';
 
 describe('#sentences', () => {
-  const { wandering: document } = sampleDocument;
-  const sentenceOne = 'i would promote introduce a constant succession and hindering the path and application getting the train they cannot keep their roving i would sooner reconcile and contemplative part of the way to direct them';
+  const { wandering } = sampleDocument;
+  const generatedWanderingSentence = 'i would promote introduce a constant succession and hindering the path and application getting the train they cannot keep their roving i would sooner reconcile and contemplative part of the way to direct them';
 
   describe('curried binary interface', () => {
-    const oneSentence = sentences(document);
+    const wanderingSentence = sentences(wandering);
 
     describe('unseeded', () => {
       it('should return a nondeterministic string', () => {
-        expect(oneSentence()).to.be.a('string');
+        expect(wanderingSentence()).to.be.a('string');
       });
     });
 
     describe('seeded', () => {
       it('should return deterministic sentences', () => {
-        expect(oneSentence(1)).to.equal(sentenceOne);
-        expect(oneSentence(2)).to.equal('i have discovered no other way to find that train they seem to be glad to be done as may be avoided of our thoughts close to our thoughts by a proper and inure them');
-        expect(oneSentence(4)).to.equal('he that yet for ought i know this essay and inure them into the discovery we might be naturally of great differences that will find');
+        expect(wanderingSentence(1)).to.equal(generatedWanderingSentence);
+        expect(wanderingSentence(2)).to.equal('i have discovered no other way to find that train they seem to be glad to be done as may be avoided of our thoughts close to our thoughts by a proper and inure them');
+        expect(wanderingSentence(4)).to.equal('he that yet for ought i know this essay and inure them into the discovery we might be naturally of great differences that will find');
       });
     });
   });
 
   describe('unary interface', () => {
-    const oneNondeterministic = { document };
+    const oneNondeterministic = { document: wandering };
     const oneDeterministic = { ...oneNondeterministic, seed: 1 };
     const threeDeterministic = { ...oneDeterministic, count: 3 };
 
@@ -35,12 +35,12 @@ describe('#sentences', () => {
     });
 
     it('returns a deterministic sentence', () => {
-      expect(sentences(oneDeterministic)).to.include.members([sentenceOne]);
+      expect(sentences(oneDeterministic)).to.include.members([generatedWanderingSentence]);
     });
 
     it('returns multiple deterministic sentences', () => {
       expect(sentences(threeDeterministic)).to.include.members([
-        sentenceOne,
+        generatedWanderingSentence,
         'he that train they seem to be glad to be done as may be avoided of our thoughts close to our thoughts by a proper and inure them',
         'this wandering of attention and yet for ought i know this wandering thoughts i would promote introduce a contrary habit',
       ]);
