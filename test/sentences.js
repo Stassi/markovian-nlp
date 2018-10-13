4cea4a3b0e690dc2607975ea6021eb2c8f570576
Andreas Stassivik <andreas@stassi.net>
2018-10-13 16:15:12 -0700

    generated sentences extraction

--- test/sentences.js
+++ test/sentences.js
@@ -4,8 +4,11 @@ import sampleDocument from './sampleDocument';
 
 describe('#sentences', () => {
   const { wandering: { text: wanderingText } } = sampleDocument;
-  // TODO: generatedWanderingSentences
-  const generatedWanderingSentence = 'i would promote introduce a constant succession and hindering the path and application getting the train they cannot keep their roving i would sooner reconcile and contemplative part of the way to direct them';
+  const generatedWanderingSentence = seed => ({
+    [1]: 'i would promote introduce a constant succession and hindering the path and application getting the train they cannot keep their roving i would sooner reconcile and contemplative part of the way to direct them',
+    [2]: 'i have discovered no other way to find that train they seem to be glad to be done as may be avoided of our thoughts close to our thoughts by a proper and inure them',
+    [4]: 'he that yet for ought i know this essay and inure them into the discovery we might be naturally of great differences that will find',
+  })[seed];
 
   describe('curried binary interface', () => {
     const wanderingSentence = sentences(wanderingText);
@@ -18,9 +21,9 @@ describe('#sentences', () => {
 
     describe('seeded', () => {
       it('should return deterministic sentences', () => {
-        expect(wanderingSentence(1)).to.equal(generatedWanderingSentence);
-        expect(wanderingSentence(2)).to.equal('i have discovered no other way to find that train they seem to be glad to be done as may be avoided of our thoughts close to our thoughts by a proper and inure them');
-        expect(wanderingSentence(4)).to.equal('he that yet for ought i know this essay and inure them into the discovery we might be naturally of great differences that will find');
+        expect(wanderingSentence(1)).to.equal(generatedWanderingSentence(1));
+        expect(wanderingSentence(2)).to.equal(generatedWanderingSentence(2));
+        expect(wanderingSentence(4)).to.equal(generatedWanderingSentence(4));
       });
     });
   });
@@ -36,12 +39,12 @@ describe('#sentences', () => {
     });
 
     it('returns a deterministic sentence', () => {
-      expect(sentences(oneDeterministic)).to.include.members([generatedWanderingSentence]);
+      expect(sentences(oneDeterministic)).to.include.members([generatedWanderingSentence(1)]);
     });
 
     it('returns multiple deterministic sentences', () => {
       expect(sentences(threeDeterministic)).to.include.members([
-        generatedWanderingSentence,
+        generatedWanderingSentence(1),
         'he that train they seem to be glad to be done as may be avoided of our thoughts close to our thoughts by a proper and inure them',
         'this wandering of attention and yet for ought i know this wandering thoughts i would promote introduce a contrary habit',
       ]);
