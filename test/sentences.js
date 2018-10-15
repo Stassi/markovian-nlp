35dba8db00aace680e5659e669e4a76081e9d8a0
Andreas Stassivik <andreas@stassi.net>
2018-10-14 23:29:04 -0700

    +(distribution input), -(binary interface)

--- test/sentences.js
+++ test/sentences.js
@@ -5,8 +5,8 @@ import sampleDocument from './sampleDocument';
 describe('#sentences', () => {
   const {
     wandering: {
-      ngramsDistribution: wanderingNgramsDistribution,
-      text: wanderingText,
+      distribution,
+      document,
     },
   } = sampleDocument;
 
@@ -16,73 +16,49 @@ describe('#sentences', () => {
     [4]: 'he that yet for ought i know this essay and inure them into the discovery we might be naturally of great differences that will find',
   })[seed];
 
-  describe('curried binary interface', () => {
-    // TODO: Implement document: ngrams || text
-    const wanderingSentence = sentences(wanderingText);
+  describe('ngrams input', () => {
+    const oneNondeterministic = { distribution };
+    const oneDeterministic = { ...oneNondeterministic, seed: 1 };
+    const threeDeterministic = { ...oneDeterministic, count: 3 };
 
-    describe('unseeded', () => {
-      it('should return a nondeterministic string', () => {
-        expect(wanderingSentence()).to.be.a('string');
-      });
+    it('returns a nondeterministic sentence', () => {
+      const [sentence] = sentences(oneNondeterministic);
+      expect(sentence).to.be.a('string');
     });
 
-    describe('seeded', () => {
-      it('should return deterministic sentences', () => {
-        expect(wanderingSentence(1)).to.equal(generatedWanderingSentence(1));
-        expect(wanderingSentence(2)).to.equal(generatedWanderingSentence(2));
-        expect(wanderingSentence(4)).to.equal(generatedWanderingSentence(4));
-      });
+    it('returns a deterministic sentence', () => {
+      expect(sentences(oneDeterministic)).to.include.members([generatedWanderingSentence(1)]);
+    });
+
+    it('returns multiple deterministic sentences', () => {
+      expect(sentences(threeDeterministic)).to.include.members([
+        generatedWanderingSentence(1),
+        'he that train they seem to be glad to be done as may be avoided of our thoughts close to our thoughts by a proper and inure them',
+        'this wandering of attention and yet for ought i know this wandering thoughts i would promote introduce a contrary habit',
+      ]);
     });
   });
 
-  describe('unary interface', () => {
-    // TODO: Reduce duplication
+  describe('text input', () => {
+    const oneNondeterministic = { document };
+    const oneDeterministic = { ...oneNondeterministic, seed: 1 };
+    const threeDeterministic = { ...oneDeterministic, count: 3 };
 
-    describe('ngrams input', () => {
-      // TODO: Implement
-      const oneNondeterministic = { document: wanderingNgramsDistribution };
-      const oneDeterministic = { ...oneNondeterministic, seed: 1 };
-      const threeDeterministic = { ...oneDeterministic, count: 3 };
-
-      it('returns a nondeterministic sentence', () => {
-        const [sentence] = sentences(oneNondeterministic);
-        expect(sentence).to.be.a('string');
-      });
-
-      it('returns a deterministic sentence', () => {
-        expect(sentences(oneDeterministic)).to.include.members([generatedWanderingSentence(1)]);
-      });
-
-      it('returns multiple deterministic sentences', () => {
-        expect(sentences(threeDeterministic)).to.include.members([
-          generatedWanderingSentence(1),
-          'he that train they seem to be glad to be done as may be avoided of our thoughts close to our thoughts by a proper and inure them',
-          'this wandering of attention and yet for ought i know this wandering thoughts i would promote introduce a contrary habit',
-        ]);
-      });
+    it('returns a nondeterministic sentence', () => {
+      const [sentence] = sentences(oneNondeterministic);
+      expect(sentence).to.be.a('string');
     });
 
-    describe('text input', () => {
-      const oneNondeterministic = { document: wanderingText };
-      const oneDeterministic = { ...oneNondeterministic, seed: 1 };
-      const threeDeterministic = { ...oneDeterministic, count: 3 };
+    it('returns a deterministic sentence', () => {
+      expect(sentences(oneDeterministic)).to.include.members([generatedWanderingSentence(1)]);
+    });
 
-      it('returns a nondeterministic sentence', () => {
-        const [sentence] = sentences(oneNondeterministic);
-        expect(sentence).to.be.a('string');
-      });
-
-      it('returns a deterministic sentence', () => {
-        expect(sentences(oneDeterministic)).to.include.members([generatedWanderingSentence(1)]);
-      });
-
-      it('returns multiple deterministic sentences', () => {
-        expect(sentences(threeDeterministic)).to.include.members([
-          generatedWanderingSentence(1),
-          'he that train they seem to be glad to be done as may be avoided of our thoughts close to our thoughts by a proper and inure them',
-          'this wandering of attention and yet for ought i know this wandering thoughts i would promote introduce a contrary habit',
-        ]);
-      });
+    it('returns multiple deterministic sentences', () => {
+      expect(sentences(threeDeterministic)).to.include.members([
+        generatedWanderingSentence(1),
+        'he that train they seem to be glad to be done as may be avoided of our thoughts close to our thoughts by a proper and inure them',
+        'this wandering of attention and yet for ought i know this wandering thoughts i would promote introduce a contrary habit',
+      ]);
     });
   });
 });
