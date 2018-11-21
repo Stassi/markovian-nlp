a5c07abde1500be13d4b17751f1c1d40ce3b78a1
Andreas Stassivik <andreas@stassi.net>
2018-11-20 22:59:00 -0800

    +mergeDistributions

--- src/ngramsDistribution/ngramsDistribution.js
+++ src/ngramsDistribution/ngramsDistribution.js
@@ -1,16 +1,29 @@
 import {
   map,
   of,
+  mergeWith,
   pipe,
+  reduce,
+  sum,
 } from 'ramda';
 import applyToString from './applyToString';
 import toDistribution from './toDistribution';
 
 const stringsToDistributions = applyToString(toDistribution);
 
-const mergeDistributions = () => {
-  // TODO: Merge (sum) all distributions
-};
+// TODO: Extract composable mergeWith, rename params, remove redundant params
+const mergeDistributions = reduce(
+  (t, u) => mergeWith(
+    (v, w) => mergeWith(
+      (x, y) => sum([x, y]),
+      v,
+      w,
+    ),
+    t,
+    u,
+  ),
+  {},
+);
 
 const ngramsDistribution = pipe(
   applyToString(of),
