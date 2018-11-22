51dbb98916fa08f3c3fbf502a3258feacc36f8bf
Andreas Stassivik <andreas@stassi.net>
2018-11-21 19:46:56 -0800

    applyToStrings extraction

--- src/ngramsDistribution/applyToString.js
+++ src/ngramsDistribution/applyToString.js
@@ -2,12 +2,19 @@ import {
   identity,
   ifElse,
   is,
+  map,
+  pipe,
 } from 'ramda';
 
 const applyToString = x => ifElse(
   is(String),
   x,
   identity,
+);
+
+export const applyToStrings = pipe(
+  applyToString,
+  map,
 );
 
 export default applyToString;
