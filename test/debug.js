137e619a250fa5911f71f3ef5e0bb7feb4bec297
Andreas Stassivik <andreas@stassi.net>
2018-11-23 01:36:12 -0800

    test input fields

--- test/debug.js
+++ test/debug.js
@@ -1,35 +1,83 @@
 import { expect } from 'chai';
-import sentences from '../src/debug';
 import lovelyQuote from './lovelyQuote';
+import sentences from '../src/debug';
+
+// TODO: Replace with expected values once known
+const debug = '__DEBUG';
 
 // TODO: Rename
 describe('#sentences DEBUG', () => {
   const { compoundDistribution: corpus } = lovelyQuote;
 
   describe('seeded', () => {
-    const { generated, seed } = sentences({
-      corpus,
-      seed: 1,
+    const seed = 1;
+
+    describe('returned values', () => {
+      const {
+        iterations,
+        generated,
+        seed: seedOut,
+      } = sentences({ corpus, seed });
+
+      it('should include the iteration count', () => {
+        expect(iterations).to.equal(debug);
+      });
+
+      it('should include deterministic generated text', () => {
+        expect(generated).to.equal(debug);
+      });
+
+      it('should include the provided seed', () => {
+        expect(seedOut).to.equal(seed);
+      });
     });
 
-    it('NEEDS A NAME', () => {
-      expect(generated).to.equal('debug');
+    describe('count: 10', () => {
+      const options = {
+        corpus,
+        seed,
+        count: 10,
+      };
+
+      describe('formatted', () => {
+        const { generated } = sentences(options);
+
+        it('should generate 10 formatted sentences', () => {
+          expect(generated).to.equal(debug);
+        });
+      });
+
+      describe('unformatted', () => {
+        const { generated } = sentences({ ...options, format: false });
+
+        it('should generate 10 unformatted sentences', () => {
+          expect(generated).to.equal(debug);
+        });
+      });
     });
 
-    it('NEEDS A NAME', () => {
-      expect(seed).to.equal(1);
+    describe('words: 10', () => {
+      const { generated } = sentences({
+        corpus,
+        seed,
+        words: 10,
+      });
+
+      it('should generate a 10-word sentence', () => {
+        expect(generated).to.equal(debug);
+      });
     });
   });
 
   describe('unseeded', () => {
-    const { generated, seed } = sentences({ corpus });
+    const { generated, seed: seedOut } = sentences({ corpus });
 
-    it('NEEDS A NAME', () => {
-      expect(generated).to.equal('debug');
+    it('should generate nondeterministic text', () => {
+      expect(generated).to.equal(debug);
     });
 
-    it('NEEDS A NAME', () => {
-      expect(seed).to.equal(1);
+    it('should return a numeric seed', () => {
+      expect(seedOut).to.be.a('number');
     });
   });
 });
