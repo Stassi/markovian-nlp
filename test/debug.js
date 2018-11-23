07708af9ec2508e0260ac1a9059a70f88f1ccb88
Andreas Stassivik <andreas@stassi.net>
2018-11-23 00:23:38 -0800

    debug distribution tests initialized

--- test/debug.js
+++ test/debug.js
@@ -1,9 +1,35 @@
 import { expect } from 'chai';
 import sentences from '../src/debug';
+import lovelyQuote from './lovelyQuote';
 
 // TODO: Rename
 describe('#sentences DEBUG', () => {
-  it('should be a function', () => {
-    expect(sentences).to.be.a('function');
+  const { compoundDistribution } = lovelyQuote;
+
+  describe('seeded', () => {
+    const { generated, seed } = sentences({
+      corpus: compoundDistribution,
+      seed: 1,
+    });
+
+    it('NEEDS A NAME', () => {
+      expect(generated).to.equal('debug');
+    });
+
+    it('NEEDS A NAME', () => {
+      expect(seed).to.equal(1);
+    });
+  });
+
+  describe('unseeded', () => {
+    const { generated, seed } = sentences({ corpus: compoundDistribution });
+
+    it('NEEDS A NAME', () => {
+      expect(generated).to.equal('debug');
+    });
+
+    it('NEEDS A NAME', () => {
+      expect(seed).to.equal(1);
+    });
   });
 });
