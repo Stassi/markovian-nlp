462cd8a110b8df6e4659d605a675f4ff1a046069
Andreas Stassivik <andreas@stassi.net>
2018-11-23 21:25:08 -0800

    stringsToDistributions simplified

--- src/ngramsDistribution/stringsToDistributions.js
+++ src/ngramsDistribution/stringsToDistributions.js
@@ -1,55 +1,14 @@
 import {
-  applySpec,
+  is,
+  map,
   pipe,
-  reduce,
+  when,
 } from 'ramda';
-import applyToStrings from './applyToString';
-import { bigrams, unigrams } from '../distributions';
-import ngrams from '../ngrams';
+import toDistribution from './toDistribution';
 
-const toDistribution = pipe(
-  applySpec(ngrams),
-  ({
-    bigrams: bigramsData,
-    unigrams: {
-      end: endUnigrams,
-      start: startUnigrams,
-      ...props
-    },
-  }) => ({
-    ...props,
-    endCount: unigrams(endUnigrams),
-    followingCounts: bigrams(bigramsData),
-    startCount: unigrams(startUnigrams),
-  }),
-  ({
-    followingCounts,
-    endCount,
-    startCount,
-    ...props
-  }) => ({
-    ...props,
-    unigramCounts: unigram => ({
-      [unigram]: {
-        ...followingCounts(unigram),
-        _end: endCount(unigram),
-        _start: startCount(unigram),
-      },
-    }),
-  }),
-  ({
-    unigramCounts,
-    all: allUnigrams,
-  }) => reduce(
-    (acc, unigram) => ({
-      ...acc,
-      ...unigramCounts(unigram),
-    }),
-    {},
-    allUnigrams,
-  ),
-);
-
-const stringsToDistributions = applyToStrings(toDistribution);
+const isString = is(String);
+const whenString = when(isString);
+const mapWhenString = pipe(whenString, map);
+const stringsToDistributions = mapWhenString(toDistribution);
 
 export default stringsToDistributions;
