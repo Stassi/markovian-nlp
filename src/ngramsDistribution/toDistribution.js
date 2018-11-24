506b34559d9ef7c54ed2b54e8f511f7840270a4e
Andreas Stassivik <andreas@stassi.net>
2018-11-23 21:24:22 -0800

    +toDistribution

---
+++ src/ngramsDistribution/toDistribution.js
@@ -0,0 +1,52 @@
+import {
+  applySpec,
+  pipe,
+  reduce,
+} from 'ramda';
+import { bigrams, unigrams } from '../distributions';
+import ngrams from '../ngrams';
+
+const toDistribution = pipe(
+  applySpec(ngrams),
+  ({
+    bigrams: bigramsData,
+    unigrams: {
+      end: endUnigrams,
+      start: startUnigrams,
+      ...props
+    },
+  }) => ({
+    ...props,
+    endCount: unigrams(endUnigrams),
+    followingCounts: bigrams(bigramsData),
+    startCount: unigrams(startUnigrams),
+  }),
+  ({
+    followingCounts,
+    endCount,
+    startCount,
+    ...props
+  }) => ({
+    ...props,
+    unigramCounts: unigram => ({
+      [unigram]: {
+        ...followingCounts(unigram),
+        _end: endCount(unigram),
+        _start: startCount(unigram),
+      },
+    }),
+  }),
+  ({
+    unigramCounts,
+    all: allUnigrams,
+  }) => reduce(
+    (acc, unigram) => ({
+      ...acc,
+      ...unigramCounts(unigram),
+    }),
+    {},
+    allUnigrams,
+  ),
+);
+
+export default toDistribution;
