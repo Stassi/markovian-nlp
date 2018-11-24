a49e76044bc6c1e5fe4e9df8bb8da4ee4aec2934
Andreas Stassivik <andreas@stassi.net>
2018-11-23 21:00:55 -0800

    +TODO

--- src/ngramsDistribution/applyToString.js
+++ src/ngramsDistribution/applyToString.js
@@ -6,12 +6,14 @@ import {
   pipe,
 } from 'ramda';
 
+// TODO: Replace with whenString (using R.when)
 export const applyToString = x => ifElse(
   is(String),
   x,
   identity,
 );
 
+// TODO: Replace with mapWhenString
 export const applyToStrings = pipe(
   applyToString,
   map,
