86019693dee2cc943f03ceb8d99c8c169a72ee7e
Andreas Stassivik <andreas@stassi.net>
2018-11-26 03:50:54 -0800

    -redundant name

--- src/seed/numericize.js
+++ src/seed/numericize.js
@@ -1,9 +1,9 @@
 import { is, unless } from 'ramda';
-import { evolveSeedProp } from '../random';
+import evolve from './evolve';
 
 const isNumber = is(Number);
 const numericSeed = ({ seed }) => isNumber(seed);
 const unlessNumericSeed = unless(numericSeed);
-const numericizeSeed = unlessNumericSeed(evolveSeedProp);
+const numericize = unlessNumericSeed(evolve);
 
-export default numericizeSeed;
+export default numericize;
