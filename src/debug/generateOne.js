022c816104cca4fa274f6d4ae1625b6616ad2f37
Andreas Stassivik <andreas@stassi.net>
2018-11-26 18:39:22 -0800

    +(relevantKeys, initializeProps)

--- src/debug/generateOne.js
+++ src/debug/generateOne.js
@@ -1,10 +1,26 @@
-const generateOne = ({ ...props }) => {
-  // TODO: Implement
-  console.log(props);
-  return 'DEBUG';
-};
+import { pick, pipe } from 'ramda';
 
-// TODO: Init once per sentence (from count)
-// const iterationsInit = ({ ...props }) => ({ ...props, iterations: 0 });
+const relevantKeys = pick([
+  'corpus',
+  'iterationLimit',
+  'seed',
+  'words',
+]);
+
+const initializeProps = ({ ...props }) => ({
+  ...props,
+  iterations: 0,
+  sentence: [],
+});
+
+const generateOne = pipe(
+  relevantKeys,
+  initializeProps,
+  ({ ...props }) => {
+    // TODO: Implement
+    console.log(props);
+    return 'DEBUG';
+  },
+);
 
 export default generateOne;
