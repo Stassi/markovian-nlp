2961fbe8e56024f2d6b865ff8212e5e04e6b10df
Andreas Stassivik <andreas@stassi.net>
2018-12-05 00:10:22 -0800

    (findStartgram, followingUnigram) import

---
+++ src/debug/followingUnigram.js
@@ -0,0 +1,24 @@
+import {
+  pipe,
+  reduce,
+  toPairs,
+} from 'ramda';
+import { weighted as weightedRandom } from '../random';
+
+// TODO: Rename
+const followingUnigram = transformWeight => pipe(
+  toPairs,
+  reduce(
+    ({ values, weights }, [value, weight]) => ({
+      values: [...values, value],
+      weights: [...weights, transformWeight(weight)],
+    }),
+    {
+      values: [],
+      weights: [],
+    },
+  ),
+  weightedRandom,
+);
+
+export default followingUnigram;
