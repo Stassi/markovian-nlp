c138c5dd1ea9932de788310be398b5aad3ff5be6
Andreas Stassivik <andreas@stassi.net>
2018-12-10 04:04:17 -0800

    bigram composition (work in progress)

--- src/debug/bigram.js
+++ src/debug/bigram.js
@@ -1,7 +1,78 @@
-// TODO: Implement
-const debug = () => 'DEBUG';
+import {
+  last,
+  omit,
+  pipe,
+  prop,
+  reduce,
+  toPairs,
+} from 'ramda';
+import { weighted as weightedRandom } from '../random';
 
-const bigram = ({
+// TODO: Rename
+const findBigram = pipe(
+  prop('unigrams'),
+  last,
+  prop,
+);
+
+const corpusProp = prop('corpus');
+const omitEnd = omit(['_end']);
+// TODO: Rename
+const getDistribution = x => pipe(
+  corpusProp,
+  x,
+  omitEnd,
+);
+
+// TODO: Rename
+const bigramDistribution = pipe(
+  findBigram,
+  getDistribution,
+);
+
+// TODO: Rename, merge with unseeded (eliminate distribution prop)
+const debugThree = ({
+  ...props,
+}) => {
+  const res = {
+    ...props,
+    distribution: bigramDistribution(props)(props),
+  };
+  return res;
+};
+
+// TODO: Rename
+const getUnseeded = pipe(
+  toPairs,
+  reduce(
+    ({ values, weights }, [value, weight]) => ({
+      values: [...values, value],
+      weights: [...weights, weight],
+    }),
+    {
+      values: [],
+      weights: [],
+    },
+  ),
+  weightedRandom,
+);
+
+// TODO: Rename
+const debugTwo = ({
+  distribution,
+  ...props
+}) => {
+  const res = {
+    ...props,
+    unseeded: getUnseeded(distribution),
+  };
+  // TODO: Select most recent unigram as corpus index
+  console.log(res);
+  return res;
+};
+
+// TODO: Rename
+const debug = ({
   unigrams,
   ...props
 }) => {
@@ -9,10 +80,16 @@ const bigram = ({
     ...props,
     unigrams: [
       ...unigrams,
-      debug(),
+      'DEBUG',
     ],
   };
   return res;
 };
+
+const bigram = pipe(
+  debugThree,
+  debugTwo,
+  debug,
+);
 
 export default bigram;
