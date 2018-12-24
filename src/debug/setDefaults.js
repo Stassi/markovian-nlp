eaa8b66167adc094f270f1a858bf8a9a14b03f2a
Andreas Stassivik <andreas@stassi.net>
2018-12-24 03:42:52 -0800

    +iterations prop

--- src/debug/setDefaults.js
+++ src/debug/setDefaults.js
@@ -1,9 +1,9 @@
-// TODO: Remove non-default parameters (i.e.: { generated })
 const setDefaults = ({
   count = 1,
   format = true,
   generated = [],
   iterationLimit = 999999999,
+  iterations = [],
   words = 15,
   ...props
 }) => ({
@@ -12,6 +12,7 @@ const setDefaults = ({
   format,
   generated,
   iterationLimit,
+  iterations,
   words,
 });
 
