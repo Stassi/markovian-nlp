522673fe3947e5c7d48da54acd650b5dd8c85c2c
Andreas Stassivik <andreas@stassi.net>
2019-01-10 18:01:58 -0800

    test signature updated

--- test/debug.js
+++ test/debug.js
@@ -2,95 +2,41 @@ import { expect } from 'chai';
 import lovelyQuote from './lovelyQuote';
 import sentences from '../src/debug';
 
-// TODO: Replace with expected values once known
-const debug = '__DEBUG';
-
 // TODO: Rename
 describe('#sentences DEBUG', () => {
   // TODO: Test corpus String
   const { compoundDistribution: corpus } = lovelyQuote;
-  const words = 4;
+  const length = 4;
 
   describe('seeded', () => {
     const seed = 1;
 
-    describe('count: 10', () => {
+    describe('count: 1', () => {
       const options = {
         corpus,
+        length,
         seed,
-        words,
-        count: 10,
+        count: 1,
       };
-      const {
-        generated,
-        iterations,
-        seed: seedOut,
-      } = sentences(options);
+      const [
+        {
+          iterations,
+          sentence,
+          seed: seedOut,
+        },
+      ] = sentences(options);
 
-      it('should generate 10 formatted sentences', () => {
-        expect(generated).to.have.ordered.members([
-          "What a lovely day.",
-          "Oh what a day.",
-          "Oh what a day.",
-          "Oh what a day.",
-          "Oh what a day.",
-          "Oh what a day.",
-          "Oh what a day.",
-          "Oh what a day.",
-          "Oh what a day.",
-          "Oh what a day.",
-        ]);
+      it('should generate a deterministic sentence', () => {
+        expect(sentence).to.equal('__DEBUG__');
       });
 
-      it('should include 10 separate iteration counts', () => {
-        expect(iterations).to.have.ordered.members([
-          1,
-          4,
-          2,
-          3,
-          1,
-          1,
-          3,
-          1,
-          2,
-          4,
-        ]);
+      it('should include the iteration count', () => {
+        expect(iterations).to.equal('__DEBUG__');
       });
 
       it('should include the provided seed', () => {
         expect(seedOut).to.equal(seed);
       });
-
-      describe('unformatted', () => {
-        const { generated } = sentences({ ...options, format: false });
-
-        it('should generate 10 unformatted sentences', () => {
-          expect(generated).to.equal(debug);
-        });
-      });
-    });
-  });
-
-  describe('unseeded', () => {
-    const {
-      generated: [generated],
-      iterations: [iterations],
-      seed: seedOut,
-    } = sentences({
-      corpus,
-      words,
-    });
-
-    it('should include numeric iterations', () => {
-      expect(iterations).to.be.a('number');
-    });
-
-    it('should generate nondeterministic text', () => {
-      expect(generated).to.be.a('string');
-    });
-
-    it('should return a numeric seed', () => {
-      expect(seedOut).to.be.a('number');
     });
   });
 });
