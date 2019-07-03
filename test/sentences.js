7cd93c198f3b92271c13f0748630e104537bb405
Andreas Stassivik <andreas@stassi.net>
2019-07-03 02:09:24 -0700

    +implausible outcomes

--- test/sentences.js
+++ test/sentences.js
@@ -11,7 +11,7 @@ describe('#sentences', () => {
   } = sampleDocument;
 
   const generatedWanderingSentence = seed => ({
-    [1]: 'i would promote introduce a constant succession and hindering the path and application getting the train they cannot keep their roving i would sooner reconcile and contemplative part of the way to direct them',
+    [1]: 'this wandering thoughts i know this may be able to reject them from straggling',
     [2]: 'i have discovered no other way to find that train they seem to be glad to be done as may be avoided of our thoughts close to our thoughts by a proper and inure them',
     [4]: 'he that yet for ought i know this essay and inure them into the discovery we might be naturally of great differences that will find',
   })[seed];
@@ -33,8 +33,8 @@ describe('#sentences', () => {
     it('returns multiple deterministic sentences', () => {
       expect(sentences(threeDeterministic)).to.include.members([
         generatedWanderingSentence(1),
-        'he that train they seem to be glad to be done as may be avoided of our thoughts close to our thoughts by a proper and inure them',
-        'this wandering of attention and yet for ought i know this wandering thoughts i would promote introduce a contrary habit',
+        'to be most useful to our minds as perhaps may come into the studious and hinder them and keep their wandering thoughts quite from taking off our care in the conduct of it can and application',
+        'this wandering of this is not the application getting the train they endeavor their roving i think it i suppose may be imagined ; or beating for ought i am satisfied is not angry chiding or confusion can and unsought ideas that even when they would sooner reconcile and going before them',
       ]);
     });
   });
@@ -56,8 +56,8 @@ describe('#sentences', () => {
     it('returns multiple deterministic sentences', () => {
       expect(sentences(threeDeterministic)).to.include.members([
         generatedWanderingSentence(1),
-        'he that train they seem to be glad to be done as may be avoided of our thoughts close to our thoughts by a proper and inure them',
-        'this wandering of attention and yet for ought i know this wandering thoughts i would promote introduce a contrary habit',
+        'to be most useful to our minds as perhaps may come into the studious and hinder them and keep their wandering thoughts quite from taking off our care in the conduct of it can and application',
+        'this wandering of this is not the application getting the train they endeavor their roving i think it i suppose may be imagined ; or beating for ought i am satisfied is not angry chiding or confusion can and unsought ideas that even when they would sooner reconcile and going before them',
       ]);
     });
   });
